943e433e8fcf2b351250c17290528fdfcdce398d
farzana <farzana09bd@gmail.com>
2021-06-02 15:37:32 +0600

    Crate database for country, state, address

---
+++ core/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Country(models.Model):
+    """"Country object"""
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    code = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ("latitude", "longitude")
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    """"State object"""
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    """"Address object"""
+    name = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=50, null=True)
+    road_number = models.IntegerField(null=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
