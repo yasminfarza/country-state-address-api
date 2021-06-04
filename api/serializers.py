cca34efba4e751e10eb5da1a2e449fd9cd8a2a4e
farzana <farzana09bd@gmail.com>
2021-06-04 23:32:20 +0600

    Create address api

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Country, State
+from core.models import Country, State, Address
 
 
 class CountrySerializer(serializers.ModelSerializer):
@@ -19,3 +19,22 @@ class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
         fields = ('id', 'name', 'country')
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    """Serializer for Address objects"""
+
+    state = serializers.StringRelatedField()
+
+    class Meta:
+        model = Address
+        fields = ('id', 'name', 'house_number', 'road_number', 'state')
+
+
+class AddressDetailSerializer(AddressSerializer):
+    """Serializer for details of address"""
+
+    def to_representation(self, instance):
+        rep = super(AddressDetailSerializer, self).to_representation(instance)
+        rep['country'] = Country.objects.get(state=instance.state).name
+        return rep
